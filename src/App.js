7c1cadc16bef0d120b21ccca9e3202066b38c999
rakshitsoni2222 <69080145+rakshitsoni2222@users.noreply.github.com>
2021-02-04 11:14:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  let array = ["red", "violet", "green"];
+  let [variable, setVariable] = useState("");
+
+  let clickFunction = () => {
+    setVariable("loading");
+    setTimeout(() => {
+      let result = array[Math.floor(Math.random() * array.length)];
+      setVariable(result);
+    }, 500);
+  };
+
+  return (
+    <div className="App">
+      <input
+        style={{ color: variable }}
+        value={variable}
+        className="input"
+        disabled
+      />
+      <button onClick={clickFunction} className="btn">
+        GET
+      </button>
+    </div>
+  );
+}
